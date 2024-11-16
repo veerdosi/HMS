9bd6f7279c971f7cc26e29e779b14fda050ffe0c
DHEERAJ <126307551+J-Dheeraj@users.noreply.github.com>
2024-11-16 21:29:14 +0800

    Add files via upload

---
+++ Pharmacist.java
@@ -0,0 +1,52 @@
+// File: Pharmacist.java
+
+import java.util.List;
+
+public class Pharmacist extends User implements InventoryManageable {
+    private String pharmacistID;
+
+    // Constructor
+    public Pharmacist(String loginID, String password, String pharmacistID) {
+        super(loginID, password);
+        this.pharmacistID = pharmacistID;
+    }
+
+    // Method to update stock of a specific medicine
+    @Override
+    public void updateStock(String medicineID, int quantity) {
+        System.out.println("Updating stock for Medicine ID: " + medicineID + " by " + quantity);
+        // Logic to update stock, possibly interacting with an Inventory class
+    }
+
+    // Submit a replenishment request for a medicine
+    @Override
+    public void submitReplenishmentRequest(Medicine medicine, int quantity) {
+        System.out.println("Submitting replenishment request for Medicine: " + medicine.getName() + " with quantity: " + quantity);
+        // Logic to create and submit a ReplenishmentRequest
+    }
+
+    // Approve a replenishment request
+    @Override
+    public void approveReplenishmentRequest(ReplenishmentRequest request) {
+        System.out.println("Approving replenishment request for Medicine ID: " + request.getMedicineID());
+        request.approve();
+    }
+
+    // Method to view appointment outcome records (Placeholder for now)
+    public List<AppointmentOutcomeRecord> viewAppointmentOutcomeRecords() {
+        System.out.println("Viewing appointment outcome records...");
+        // Return mock list or fetched list of records
+        return null;
+    }
+
+    // Method to update prescription status
+    public void updatePrescriptionStatus(Prescription prescription) {
+        System.out.println("Updating prescription status for Prescription ID: " + prescription.getPrescriptionID());
+        prescription.setPrescriptionStatus("dispensed");
+    }
+
+    // Display pharmacist information
+    public void displayPharmacistInfo() {
+        System.out.println("Pharmacist ID: " + pharmacistID + ", Login ID: " + getLoginID());
+    }
+}
