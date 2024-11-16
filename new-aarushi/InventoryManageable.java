7556640c1c58dee97ac725b108481e7da037350e
aarushigambhir <aarushigambhir04@gmail.com>
2024-11-16 22:52:01 +0800

    Delete new-aarushi/InventoryManageable.java

--- new-aarushi/InventoryManageable.java
+++
@@ -1,6 +0,0 @@
-
-public interface InventoryManageable {
-    void updateMedicineStock(Medicine medicine, int quantity);
-    void submitReplenishmentRequest(Medicine medicine, int quantity);
-    void approveReplenishmentRequest(ReplenishmentRequest request);
-}
