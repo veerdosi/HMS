65e0df6ce2445d0068ec9c6d0c60ced9454d59d1
Veer Dosi <veerdosi00@gmail.com>
2024-11-24 06:28:54 +0800

    removed archive folder

--- archive/new-hana/src/RequestStatus.java
+++
@@ -1,5 +0,0 @@
-public enum RequestStatus {
-    PENDING,
-    APPROVED,
-    REJECTED
-}
--- archive/new-hana/src/TypeOfService.java
+++
@@ -1,9 +0,0 @@
-public enum TypeOfService {
-    CONSULTATION,
-    LAB_SERVICES,
-    RADIOLOGY,
-    EMERGENCY_SERVICES,
-    VACCINATION,
-    FOLLOW_UP,
-    HEALTH_CHECKUP
-}
