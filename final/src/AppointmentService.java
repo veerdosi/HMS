feb7bdcd2ec77eda6c610b2a0569cdf9398eb077
Veer Dosi <veerdosi00@gmail.com>
2024-11-24 09:08:30 +0800

    basic moving around

--- final/src/AppointmentService.java
+++ final/src/AppointmentService.java
@@ -10,7 +10,6 @@
  */
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class AppointmentService {
     private DoctorService doctorService;
--- final/src/Doctor.java
+++ final/src/Doctor.java
@@ -1,5 +1,4 @@
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
